e0cb7879154f570811b056e22b6e53926b74f7b3
GKoutroumpis <GKoutroumpis@cipher-sys.com>
2019-01-29 13:05:36 -0500

    added about component, made changes to app

--- george-site/ClientApp/src/App.js
+++ george-site/ClientApp/src/App.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { About } from './components/About';
 
 export default class App extends Component {
   displayName = App.name
@@ -12,7 +12,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
+        <Route path='/about' component={About} />
         <Route path='/fetchdata' component={FetchData} />
       </Layout>
     );
---
+++ george-site/ClientApp/src/components/About.js
@@ -0,0 +1,58 @@
+﻿import React, { Component } from 'react';
+import logo from '../profile-picture.jpeg';
+import { Col, Row, Image } from 'react-bootstrap';
+import './About.css';
+
+export class About extends Component {
+    displayName = About.name
+
+    state = {
+        name: "George Koutroumpis",
+        description: "Software Engineer",
+        location: "Annapolis, MD"
+    }
+    //constructor(props) {
+    //    super(props);
+    //    this.state = "test";
+    //    debugger;
+    //}
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col md={3}>
+                        <img src={logo} />
+                    </Col>
+                    <Col md={9}>
+                        <h1>{this.state.name}</h1>
+                        <h4>{this.state.description}</h4>
+                        <h4>{this.state.location}</h4>
+                        <a className="linkedin-link" target="_blank" href="https://www.linkedin.com/in/george-koutroumpis-065629122/">
+                            <span className="fa-stack fa-lg">
+                                <i className="fas fa-circle fa-stack-2x"></i>
+                                <i className="fab fa-linkedin-in fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                        <a className="github-link" target="_blank" href="https://github.com/gkoutr">
+                            <span className="fa-stack fa-lg">
+                                <i className="fas fa-circle fa-stack-2x"></i>
+                                <i className="fab fa-github fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </Col>
+                </Row>
+                
+               
+             </div>
+            );
+    }
+};
+
+About.defaultProps = {
+    name: "George",
+    age: "26"
+}
