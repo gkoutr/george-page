1f49c8033485ff00fa7c999285b04269c8366212
George Koutroumpis <gjkoutroumpis@gmail.com>
2019-11-09 09:30:13 -0500

    updated dependencies

--- george-site/Controllers/AboutController.cs
+++ george-site/Controllers/AboutController.cs
@@ -14,19 +14,7 @@ namespace george_site.Controllers
     [Route("api/[controller]")]
     public class AboutController : Controller
     {
-  //      const filePath = path.resolve(__dirname, './build', 'index.html')
-  //fs.readFile(filePath, 'utf8', function(err, data)
-  //      {
-  //          if (err)
-  //          {
-  //              return console.log(err);
-  //          }
-  //          data = data.replace(/\$OG_TITLE / g, 'About Page');
-  //          data = data.replace(/\$OG_DESCRIPTION / g, "About page description");
-  //          result = data.replace(/\$OG_IMAGE / g, 'https://i.imgur.com/V7irMl8.png');
-  //          response.send(result);
-  //      });
-        
+
         [HttpGet("[action]")]
         public IEnumerable<Job> GetJobs()
         {
